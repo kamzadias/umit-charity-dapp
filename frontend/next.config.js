e5185b96e0edd3910ac861aa7fb033755a3c14c2
Dias Kamza <kamzadias31@gmail.com>
2025-04-26 19:29:17 +0500

    chore: add .env.example files for frontend and blockchain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    productionBrowserSourceMaps: false,
+    output: 'export',
+
+    experimental: {
+        images: {
+            unoptimized: true
+        }
+    },
+
+    env: {
+        NEXT_PUBLIC_CLIENT_ID: process.env.CLIENT_ID
+    },
+
     webpack: (config) => {
         config.externals.push('pino-pretty', 'lokijs', 'encoding');
         return config;
